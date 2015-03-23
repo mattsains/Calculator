b75edb5ca8b0884113f1245daf8e58b70b3d3c29
Matthew Sainsbury <matthew@sainsbury.za.net>
2015-03-23 22:14:33 +0200

    Added a grammar test program

---
+++ main.rb
@@ -0,0 +1,29 @@
+require 'polyglot'
+require 'treetop'
+
+def print_tree t, depth = 0
+  if t !=nil
+    print " "*depth
+    puts (t.extension_modules[0].inspect.split "::")[1]
+    
+    if t.elements != nil
+      for c in t.elements
+        print_tree(c, depth+1) unless c.extension_modules[0] == nil
+      end
+    end
+  end
+end
+
+while true
+  Treetop.load 'grammar'
+  parser = ExpressionParser.new
+  print "> "
+  input = gets.chomp
+  next if input == 'r'
+  result = parser.parse input
+  if result == nil
+    puts parser.failure_reason
+  else
+    puts print_tree result
+  end
+end
